3567481df47f10ce26e6e5f0aba4b499ecefd106
bendez2 <tema98_15@mail.ru>
2022-10-12 09:08:19 +0500

    Доработка

--- AuthUser.xaml.cs
+++ AuthUser.xaml.cs
@@ -57,7 +57,7 @@ namespace Nastol
 
             HttpClient client = new HttpClient();
             content.Headers.ContentType = MediaTypeHeaderValue.Parse(@"application/json");
-            HttpResponseMessage response = await client.PostAsync(urlLocal, content);
+            HttpResponseMessage response = await client.PostAsync(url, content);
            
             if (response.StatusCode == HttpStatusCode.OK)
             {
