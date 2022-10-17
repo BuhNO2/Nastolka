08e7dac35802692b8eb4e3202037c193bd1020cf
bendez2 <tema98_15@mail.ru>
2022-10-17 21:23:41 +0500

    Исправление ошибок

---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace Nastol
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string Enterprice { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Name { get; set; }
+
+        public string Patronomic { get; set; }
+
+        public string DateofBirth { get; set; }
+
+        public int RoleId { get; set; }
+    }
+}
---
+++ Pages/AuthUser.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System.Windows;
+
+namespace Nastol
+{
+    public partial class AuthUser : Window
+    {
+        RequestApi requestApi = new RequestApi();
+
+        public AuthUser()
+        {
+            InitializeComponent();
+            ResizeMode = ResizeMode.NoResize;
+            ResizeMode = ResizeMode.CanMinimize;
+        }
+
+        private async void AuthClick(object sender, RoutedEventArgs e)
+        {
+            AuthButt.IsEnabled = false;
+
+            string json = JsonConvert.SerializeObject(new User()
+            {
+                ID = 0,
+                Login = LoginUser.Text,
+                Password = HashingString.HashingPassword(PassUser.Password),
+                Enterprice = "",
+                Surname = "",
+                Name = "",
+                Patronomic = "",
+                DateofBirth = "",
+                RoleId = 0
+            });
+
+            string authUser = await requestApi.SendAPostRequstAsync("Auth", json);
+
+            if (authUser != null)
+            {
+                CurrentUser.user = JsonConvert.DeserializeObject<User>(authUser);
+                Window secondWindow = new Menu();
+                secondWindow.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show(" Неверный логин или пароль!", "Ошибка авторизации", MessageBoxButton.OK, MessageBoxImage.Error);
+                AuthButt.IsEnabled = true;
+            }
+        }
+    }
+}
